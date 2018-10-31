8a2af2a01e92a69fff2b0b214fff834387c42fa8
gngupta <gorakh2010@gmail.com>
2018-10-31 17:16:08 +0100

    Vue.js learning

---
+++ src/components/Test.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    {{msg}}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Test",
+    data() {
+      return {
+        msg: "Welcome to Your Vue.js App"
+      };
+    }
+  };
+</script>
